9f572785e0a3472174cb5f446db0a62b6c91d775
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-23 01:18:37 +0600

    server created

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express()
+const port = process.env.PORT || 2000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`Running server at port : ${port}`)
+})
+
+
+app.listen(port, () => {
+    console.log(`Running server at : https://localhost:${port}`)
+})
+
